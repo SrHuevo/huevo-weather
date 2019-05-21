595d4912b6267971de0cc52c3a333acdc829336e
srhuevo <garoz.daniel@gmail.com>
2019-05-21 21:18:53 +0200

    add https

--- dist/utils/environment.js
+++ dist/utils/environment.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     openweathermap: {
         API_KEY: '6dcf6a37658592f78d6de68a168afc72',
-        url: 'http://api.openweathermap.org',
+        url: 'https://api.openweathermap.org',
         resources: {
             currentWeather: '/data/2.5/weather',
             coordsWeather: '/data/2.5/find'
--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,7 +1,7 @@
 export default {
   openweathermap: {
     API_KEY: '6dcf6a37658592f78d6de68a168afc72', // replace CI for var env
-    url: 'http://api.openweathermap.org',
+    url: 'https://api.openweathermap.org',
     resources: {
       currentWeather: '/data/2.5/weather',
       coordsWeather: '/data/2.5/find'
